0a4b4bf70a380a0a37cf0fbdcc0d673edd6239d3
crissotto <carlosrissotto48@gmail.com>
2024-10-22 18:46:50 -0300

    create the preference adding the name, description and restriction to the file. implement the model to the db

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -2,7 +2,28 @@
 
 class PreferencesController < ApplicationController
   def index
-    @preferences = current_user.preferences
+    @preferences = current_user.preferences.includes(:user)
     @pagy, @records = pagy(@preferences)
+  end
+
+  def create # create a function to create a new preference
+
+    @preference = current_user.preferences.new(preference_params) #  create a new preference for the current user
+
+    if @preference.save  # if the preference is saved successfully
+
+      redirect_to preferences_path, notice: 'Preference was successfully created.'  # redirect to the preferences page with a success message
+
+    else
+      render :new, status:  :unprocessable_entity  # if the preference is not saved, render the new template with an unprocessable entity status
+
+    end
+  end
+
+  private
+
+  def preference_params 
+    params.require(:preference).permit(:name, :description, :restriction)  # permit the name, description and restriction fields for the preference
+
   end
 end
