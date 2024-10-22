14d7188dae5fff31d2c0be86f392cf6e7e34c50b
crissotto <carlosrissotto48@gmail.com>
2024-10-22 12:55:40 -0300

    Add some gems to the project also add an user to the db

---
+++ app/models/preference.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: preferences
+#
+#  id          :bigint           not null, primary key
+#  name        :string
+#  description :text
+#  restriction :boolean
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+class Preference < ApplicationRecord
+    validates :name, presence: true
+    validates :description, presence: true
+end
+
+
